7a158ca4d3bf91af80d951e41ecaeef302055f99
Vladyslav <kupchuk.vlad@gmail.com>
2024-07-13 18:18:56 +0300

    ADDED: upload photos to cloud storage in addNewEquipment controller

--- controllers/equipments/addNewEquipment.js
+++ controllers/equipments/addNewEquipment.js
@@ -1,9 +1,33 @@
+const fs = require("fs/promises");
+
 const { Equipment } = require("../../models");
+const { cloudinary } = require("../../helpers");
 
 const addNewEquipment = async (req, res) => {
-	const { _id: owner} = req.user;
-	const addedEquipment = (await Equipment.create({ ...req.body, owner}));
+	const { _id: owner } = req.user;
+	const arrayOfOldPaths = req.files.map(file => file.path);
+
+	const arrayOfPhotosUrl = await Promise.all(arrayOfOldPaths.map(async (path) => {
+		const { url } = await cloudinary.uploader.upload(path,
+			{
+				folder: "equipment-site"
+			})
+		
+		await fs.unlink(path)
+
+		return url;
+	}))
+
+	const addedEquipment = await Equipment.create({ ...req.body, photos: arrayOfPhotosUrl, owner});
 	res.status(201).json(addedEquipment);
 }
 
 module.exports = addNewEquipment;
+
+// {
+//     "category": "pressing",
+// 	"model": "UWDRW0867",
+// 	"photos": ["penis", "jopa"],
+// 	"features": "Обороты: 4000 м/с \n Потужність двигуна: 40 кВт",
+// 	"describe": "super cool"
+// }
